7da63f9b0bcb1a4929ecba1e8f84035700256915
Alexander Zaikov <zaikovalexander@gmail.com>
2022-11-28 19:59:38 +0700

    test

--- lib/src/main/java/custom/spring/library/CustomLibraryConfiguration.java
+++ lib/src/main/java/custom/spring/library/CustomLibraryConfiguration.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootConfiguration
 @ComponentScan
 @EnableAspectJAutoProxy
+@PropertySource("classpath:lib.properties")
 @Slf4j
 public class CustomLibraryConfiguration {
     CustomLibraryConfiguration() {
