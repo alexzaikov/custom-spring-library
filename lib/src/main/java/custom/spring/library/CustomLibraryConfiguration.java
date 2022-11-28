ab264cd7bc600660ab80e423047bf57aa2657bf4
Alexander Zaikov <zaikovalexander@gmail.com>
2022-11-28 13:32:51 +0700

    test

--- lib/src/main/java/custom/spring/library/CustomLibraryConfiguration.java
+++ lib/src/main/java/custom/spring/library/CustomLibraryConfiguration.java
@@ -4,10 +4,12 @@ package custom.spring.library;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootConfiguration
 @ComponentScan
+@EnableAspectJAutoProxy
 @Slf4j
 public class CustomLibraryConfiguration {
     CustomLibraryConfiguration() {
--- lib/src/main/java/custom/spring/library/CustomLibraryService.java
+++ lib/src/main/java/custom/spring/library/CustomLibraryService.java
@@ -1,14 +1,18 @@
 package custom.spring.library;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class CustomLibraryService {
 
+    @Value("${custom.library.value}")
+    private String propertyValue;
+
     @LogExecutionTime
     public void printSome(String val) {
-        System.out.println("!!! + " + val);
+        System.out.println(String.format("!!! %s %s", propertyValue, val));
     }
 }
